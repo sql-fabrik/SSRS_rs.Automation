205947a913518505976a1a58ea8283953de8a22c
sql-fabrik <89515631+sql-fabrik@users.noreply.github.com>
2021-08-26 12:54:02 +0200

    initial commit

---
+++ a_ReportCommand.cmd
@@ -0,0 +1,18 @@
+
+
+
+rs.exe  -i C:\_test\a_Report1.rss -s "localhost/ReportServer" -v FILENAME="C:\_test\filename.pdf" -v FORMAT="PDF" -v REPORTSERVER_FOLDER="/Reports01/Report1" -t -e Exec2005
+
+rs.exe  -i C:\_test\a_Report1.rss -s "localhost/ReportServer" -v FILENAME="C:\_test\filename.xls" -v FORMAT="Excel" -v REPORTSERVER_FOLDER="/Reports01/Report1" -t -e Exec2005
+
+------------------------------------------------------------
+
+
+rs.exe  -i C:\_test\a_Report2.rss -s "localhost/ReportServer" -v RECHNR="1088" -v FILENAME="C:\_test\Rech_1088.xls" -v FORMAT="Excel" -v REPORTSERVER_FOLDER="/Reports01/Report2" -t -e Exec2005
+rs.exe  -i C:\_test\a_Report2.rss -s "localhost/ReportServer" -v RECHNR="1088" -v FILENAME="C:\_test\Rech_1088.pdf" -v FORMAT="PDF" -v REPORTSERVER_FOLDER="/Reports01/Report2" -t -e Exec2005
+
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+
+
